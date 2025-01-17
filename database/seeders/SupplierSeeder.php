56935b35eb76aba479b81f5a2fe086aa71c07f61
Karim Aouaouda <karimaouaouda.officiel@gmail.com>
2025-01-17 14:02:01 +0100

    add seeder for address

--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Supplier;
 use Database\Factories\SupplierFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Lunar\Models\Country;
 
 class SupplierSeeder extends Seeder
 {
@@ -13,6 +15,19 @@ class SupplierSeeder extends Seeder
      */
     public function run(): void
     {
+        $countries = Country::all();
         (new SupplierFactory())->count(10)->create();
+        foreach (Supplier::all() as $supplier) {
+            $country = $countries->random();
+            $state = $country->states->random();
+            $supplier->address()->create([
+                'country' => $country->id,
+                'state' => $state->id,
+                'address_line' => fake()->text(100),
+                'latitude' => 1.2,
+                'longitude' => 1.2,
+                'city' => $state->name
+            ]);
+        }
     }
 }
